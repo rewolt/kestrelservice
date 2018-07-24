660f5844ad030da4132822067a3edc43b2852464
Bartłomiej Tomczak <rewolt90@gmail.com>
2018-07-24 19:34:54 +0200

    Added support for API. Now every project is represented by different button.

--- WebChat/wwwroot/script.js
+++ WebChat/wwwroot/script.js
@@ -1,15 +1,43 @@
-﻿var time = document.getElementById("time");
+﻿
+var time = document.getElementById("time");
 time.innerHTML = Date();
 
 var refreshFunc = setInterval(function () { setTime(); }, 1000);
-
 function setTime() {
     time.innerHTML = Date();
 }
 
-function goToWebChat() {
-    var webChatUrl = document.location.hostname + '/webchat';
-    window.location = webChatUrl;
+var xhttp = new XMLHttpRequest();
+var scheme = document.location.protocol;
+var port = document.location.port ? (":" + document.location.port) : "";
+var baseUrl = scheme + "//" + document.location.hostname + port;
+
+xhttp.open("GET", baseUrl + "/api/project", true);
+xhttp.onreadystatechange = function (ev) {
+    if (xhttp.readyState === 4 && xhttp.status === 200) {
+        handleResponse(xhttp.responseText);
+    }
+}
+xhttp.send();
+
+function handleResponse(response) {
+    var jsonResp = JSON.parse(response);
+    var container = document.getElementById("button_container");
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    for (var project of jsonResp) {
+        var button = document.createElement("button");
+        button.className = "button";
+        button.value = project;
+        button.id = "button_" + project;
+        button.onclick = function () { goToProject(button.value); };
+        button.innerHTML = "<span>" + project + "</span>";
+        container.appendChild(button);
+    }
 }
 
-
+function goToProject(projectName) {
+    window.location = baseUrl + "/" + projectName;
+}
